7cc573be7927209ed24e79b3ec624f0f74c6e54e
mertmehmet6100@gmail.com <mertmehmet6100@gmail.com>
2023-04-28 23:09:47 +0300

    Add project files.

---
+++ ntpoyun/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace ntpoyun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(283, 135);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(408, 135);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(97, 20);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(141, 174);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(97, 20);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(283, 174);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(97, 20);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(408, 174);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(97, 20);
+            this.textBox6.TabIndex = 0;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(141, 217);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(97, 20);
+            this.textBox7.TabIndex = 0;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(283, 217);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(97, 20);
+            this.textBox8.TabIndex = 0;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(408, 217);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(97, 20);
+            this.textBox9.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(428, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(542, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(542, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "label1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(542, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Location = new System.Drawing.Point(521, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Kontrol";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(769, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ ntpoyun/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ntpoyun
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+            static int[,] dizi = new int[3, 3];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            List<int> dizi2 = new List<int>();
+            dizi2.Add(1);
+            dizi2.Add(2);
+            dizi2.Add(3);
+            dizi2.Add(4);
+            dizi2.Add(5);
+            dizi2.Add(6);
+            dizi2.Add(7);
+            dizi2.Add(8);
+            dizi2.Add(9);
+            int say = 9;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int sayi= rnd.Next(1, say);
+                    dizi[i, j] = dizi2[sayi-1];
+                    dizi2.Remove(dizi2[sayi-1]);
+                    say--;
+                    
+
+                }
+            }
+            label1.Text = (dizi[0, 0] + dizi[1, 0] + dizi[2, 0]).ToString();
+            label2.Text = (dizi[1, 1] + dizi[0, 1] + dizi[2, 1]).ToString();
+            label3.Text = (dizi[0, 2] + dizi[1, 2] + dizi[2, 2]).ToString();
+            label6.Text = (dizi[0, 0] + dizi[0, 1] + dizi[0, 2]).ToString();
+            label5.Text = (dizi[1, 0] + dizi[1, 1] + dizi[1, 2]).ToString();
+            label4.Text = (dizi[2, 0] + dizi[2, 1] + dizi[2, 2]).ToString();
+            textBox1.Text = dizi[0, 0].ToString();
+            textBox2.Text = dizi[0, 1].ToString();
+            textBox3.Text = dizi[0, 2].ToString();
+            textBox4.Text = dizi[1, 0].ToString();
+            textBox5.Text = dizi[1, 1].ToString();
+            textBox6.Text = dizi[1, 2].ToString();
+            textBox7.Text = dizi[2, 0].ToString();
+            textBox8.Text = dizi[2, 1].ToString();
+            textBox9.Text = dizi[2, 2].ToString();
+            int sayac = 1;
+            while (sayac < 6)
+            {
+                int sayi=rnd.Next(1, 9);
+                string isim = "textBox" + sayi;
+                foreach (Control item in Controls)
+                {
+                    if (item is TextBox && item.Name==isim && item.Text!="")
+                    {
+                        item.Text = "";
+                        sayac++;
+                    }
+                }
+                
+            }
+            
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != dizi[0, 0].ToString())
+            {
+                textBox1.ForeColor = Color.Red;
+            }
+            if (textBox2.Text != dizi[0, 1].ToString())
+            {
+                textBox2.ForeColor = Color.Red;
+            }
+            if (textBox3.Text != dizi[0, 2].ToString())
+            {
+                textBox3.ForeColor = Color.Red;
+            }
+            if (textBox4.Text != dizi[1, 0].ToString())
+            {
+                textBox4.ForeColor = Color.Red;
+            }
+            if (textBox5.Text != dizi[1, 1].ToString())
+            {
+                textBox5.ForeColor = Color.Red;
+            }
+            if (textBox6.Text != dizi[1, 2].ToString())
+            {
+                textBox6.ForeColor = Color.Red;
+            }
+            if (textBox7.Text != dizi[2, 0].ToString())
+            {
+                textBox7.ForeColor = Color.Red;
+            }
+            if (textBox8.Text != dizi[2, 1].ToString())
+            {
+                textBox8.ForeColor = Color.Red;
+            }
+            if (textBox9.Text != dizi[2, 2].ToString())
+            {
+                textBox9.ForeColor = Color.Red;
+            }
+            int say = 0;
+            foreach (Control item in Controls)
+            {
+                if (item is TextBox && item.ForeColor==Color.Red)
+                {
+                    say++;
+                }
+            }
+            if (say == 0)
+            {
+                MessageBox.Show("Başardınız");
+            }
+            else
+            {
+                MessageBox.Show("Lütfen hatalarını düzeltiniz");
+                foreach (Control item in Controls)
+                {
+                    if (item is TextBox)
+                    {
+                        item.ForeColor = Color.Black;
+                    }
+                }
+            }
+        }
+    }
+}
